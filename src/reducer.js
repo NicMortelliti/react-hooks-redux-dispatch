c23d58ed803720be5865d78cc44db5a0cccfa663
Nic Mortelliti <npmortelliti@gmail.com>
2022-11-25 08:27:41 -0800

    Lab complete

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,5 @@
+let state = { count: 0 };
+
 function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
@@ -7,7 +9,20 @@ function changeState(state, action) {
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+function dispatch(action) {
+  state = changeState(state, action);
+  return state;
+}
 
-changeState(state, action);
+// Render to html
+function render() {
+  const app = document.querySelector("#app");
+  app.textContent = state.count;
+}
+
+// Increment state twice
+dispatch({ type: "counter/increment" });
+dispatch({ type: "counter/increment" });
+
+// Call render function
+render();
